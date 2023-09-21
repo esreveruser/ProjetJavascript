817b310929ff0a90a42b746958d1abd31946496d
Alexandre Dupont <alexandredupont@MacBook-Air-de-Alexandre.local>
2023-09-21 16:28:10 +0200

    second commit

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
 print("Hello World!")
+def firstfunction():
+    print("This is the first function")
+    for i in range(10):
+        print(i)
+    return
+
+def main():
+    firstfunction()
+    return
+
+main()
