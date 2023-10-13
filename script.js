b4d8bcb185aa397eb3b024881d9c7bff3f040456
esreveruser <alexandredupont@MacBook-Air-de-Alexandre.local>
2023-10-13 11:21:45 +0200

    [ADD] Ajout de tâches, suppression de tâches, modification des tâches, ajout et modification d'une date, checkbox=on/off

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ listes.addEventListener("click", deleteList);
 //Fonctions
 
 function addList(event) {
+    //LISTES
     //Empêche bug
     event.preventDefault();
     //Création d'une div pour l'ajout des listes
@@ -19,7 +20,7 @@ function addList(event) {
     //Ajout de la classe "liste" dans la div
     listDiv.classList.add("list");
     //Création d'un li pour l'ajout de la liste
-    const newList = document.createElement("li");
+    const newList = document.createElement("h1");
     newList.innerText = btnAddListName.value;
     //Ajout de la classe "listName" dans le li
     newList.classList.add("list-name");
@@ -29,21 +30,30 @@ function addList(event) {
     const btnDeleteList = document.createElement("button");
     //Ajout de la classe "btn-del-list" dans le bouton
     btnDeleteList.classList.add("btn-del-list");
+    btnDeleteList.innerText = "Supprimer la liste";
     //Ajout du bouton dans la div list
     listDiv.appendChild(btnDeleteList);
     //Ajout de la div list dans la liste non ordonnée "listes" 
     listes.appendChild(listDiv);
     //Réinitialisation du champs dans le placeholder
     btnAddListName.value = "";
-    //
+    //TACHES
     //Création d'un input type text pour ajouter une tâche
     const addTask = document.createElement("input");
+    //Ajout de la classe "input-add-task" dans le input
     addTask.classList.add("input-add-task");
-    // addTask.innerHTML = "remplace";
+    //Ajout du type text dans le input
+    addTask.setAttribute("type", "text");
+    //Ajout du placeholder dans le input
+    addTask.setAttribute("placeholder", "Ajouter une tâche...");
+    //Ajout du input dans la div list
     listDiv.appendChild(addTask);
-    //Création d'un input type date pour ajouter à la tâche une deadline
-    const addDate = document.createElement("input");
-    listDiv.appendChild(addTask);
+    //Création d'un bouton pour ajouter une tâche
+    const btnAddTask = document.createElement("button");
+    btnAddTask.classList.add("btn-add-task");
+    btnAddTask.innerText = "Ajouter une tâche";
+    listDiv.appendChild(btnAddTask);
+    btnAddTask.addEventListener("click", () => addingTask(event, addTask, listDiv));
 }
 
 function deleteList(event) {
@@ -53,21 +63,53 @@ function deleteList(event) {
     }
 }
 
-function addTasks(event) {
-    //Empêche bug
-    event.preventDefault();
-    //Création d'une div pour l'ajout des tâches
-    const taskDiv = document.createElement("div");
-    //Ajout de la classe "task" dans la div
-    taskDiv.classList.add("task");
-    //Création d'un li pour l'ajout de la tâche
-    const newTask = document.createElement("li");
-    newTask.innerText = btnAddTaskName.value;
-    //Ajout de la classe "taskName" dans le li
-    newTask.classList.add("task-name");
-    //Ajout du li dans la div
-    taskDiv.appendChild(newTask);
-    //Création d'un bouton pour supprimer la tâche
+function addingTask(event, taskTxt, listDiv) {
+    const newTask = document.createElement("ul");
+    newTask.classList.add("task");
+    listDiv.appendChild(newTask);
+
+    const printTask = document.createElement("p");
+    printTask.innerText = taskTxt.value;
+    newTask.appendChild(printTask);
+
     const btnDeleteTask = document.createElement("button");
-    //Ajout de la classe "btn-del-task" dans le bouton
+    btnDeleteTask.classList.add("btn-del-task");
+    btnDeleteTask.innerText = "Delete task";
+    btnDeleteTask.addEventListener("click", deleteTask);
+    newTask.appendChild(btnDeleteTask);
+    taskTxt.value = "";
+
+    const btnCheckTask = document.createElement("input");
+    btnCheckTask.setAttribute("type", "checkbox");
+    btnDeleteTask.classList.add("checkDoTask");
+    console.log(btnCheckTask.value);
+    newTask.appendChild(btnCheckTask);
+
+    const calendrierdevacance = document.createElement("input");
+    calendrierdevacance.setAttribute("type", "date");
+    newTask.appendChild(calendrierdevacance);
+
+    const modTask = document.createElement("button");
+    modTask.classList.add("btn-mod-task");
+    modTask.innerText = "Modifier tâche";
+    newTask.appendChild(modTask);
+    modTask.addEventListener("click", () => modifyTask(event, printTask, newTask, newtaskmod));
+
+
+
+
+    const newtaskmod = document.createElement("input");
+    newtaskmod.setAttribute("placeholder", "Modifier votre tâche...");
+    newTask.appendChild(newtaskmod);
+}
+
+function deleteTask(event) {
+    const itemToDel = event.target;
+    itemToDel.parentElement.remove();
+}
+
+function modifyTask(event, tasktomod, newTask, newtaskmod) {
+    const itemToMod = event.target;
+    tasktomod.innerText = newtaskmod.value;
+    newtaskmod.value = "";
 }
